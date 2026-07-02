10a9d86c956322fe8d8bb85c8b139ead8bea6d9e
Surya12125 <vvsuryapepakayala@gmail.com>
2026-07-02 21:49:04 +0530

    Implement MyService with fetch and process methods

---
+++ Week2_PL-SQL_TDD/Module4-TDD-JUnit-Mockito/Exercise3-Mockito/MyService.java
@@ -0,0 +1,16 @@
+public class MyService {
+
+    private final ExternalApi externalApi;
+
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public String fetchData() {
+        return externalApi.getData();
+    }
+
+    public void processData(String data) {
+        externalApi.sendData(data);
+    }
+}
